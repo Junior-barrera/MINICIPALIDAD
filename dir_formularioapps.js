d5f1b3007f46f59c984a3a7424db5f0e37dc280b
Junior Barrera <junior33diazbarrera@gmail.com>
2025-06-03 09:47:24 -0400

    Delete dir_formularioapps.js

--- dir_formularioapps.js
+++
@@ -1,2 +0,0 @@
-const dir_pac = "https://script.google.com/macros/s/AKfycbw9TfE8-orUBNLrpOtchgrknOEoodAbGbutAS49SukUTwMTu6t3m4LWBScCV3MoFnY/exec";
-const dir_mec = "https://script.google.com/macros/s/AKfycbzii6cfqEZB3UAg3Vx0VR2dl2_5w_RmgkjDk69zdExcm7AkwDcdgbBNMLQqqVQbBQJ_/exec";
